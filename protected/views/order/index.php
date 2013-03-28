81b21c8382143e37b02d95a2e87954a2d01ef957
hzbdesigner-macbook pro <hzbdesigner@gmail.com>
2013-03-28 20:32:02 +0800

    order

---
+++ protected/views/order/index.php
@@ -0,0 +1,65 @@
+	<!-- start: Page Title -->
+	<div id="page-title">
+
+		<div id="page-title-inner">
+
+			<!-- start: Container -->
+			<div class="container">
+
+				<h2><i class="ico-lightbulb ico-white"></i>北京科技大学预约系统</h2>
+
+			</div>
+			<!-- end: Container  -->
+
+		</div>	
+
+	</div>
+	<!-- end: Page Title -->
+	
+	<!--start: Wrapper-->
+	<div id="wrapper">
+		
+
+		<!-- end: Container  -->
+				
+		<!--start: Container -->
+    	<div class="container">
+	
+			<!-- start: Portfolio -->
+			<div id="portfolio-wrapper" class="row">
+				
+			<?php 
+
+// 				foreach ($models as $model) {
+// 					$title=$model->title;
+// 					$img=$model->despic;
+// 					$des=$model->des;
+// 					$article_id=$model->article_id;
+// 					$date=substr($model->date, 0, 10);
+// 					$author=$model->author;
+// 					$read_num=$model->read_num;
+// 					$the_column_id=$model->column_id;
+// 					$the_catalog_id=$model->catalog_id;
+// 					$url=$this->createUrl('view',array('article_id'=>$article_id, 'column_id'=>$the_column_id,'catalog_id'=>$the_catalog_id));
+// 					echo <<<EOD
+// 				<div class="span3 portfolio-item">
+// 					<div class="picture"><a href="#" title="Title"><img src="$img"></div></a>
+// 						<div class="item-description alt">
+// 							<h5><a href="$url">$title</a></h5>
+// 							<p>$des</p>
+// 						</div>
+// 						<div class="post-meta"><span><i class="mini-ico-calendar"></i>$date</span> <span><i class="mini-ico-user"></i> <a href="#">$author</a></span> <span><i class="mini-ico-comment"></i><a href="#">$read_num</a></span></div>
+// 					</div>	
+// 				</div>
+// EOD;
+				//}
+			?>		
+
+			</div>
+			<!-- end: Portfolio -->
+      	
+		</div>
+		<!--end: Container-->
+				
+	</div>
+	<!-- end: Wrapper  -->	
